169b510e9b481096810925f68090123dde63da53
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2013-10-30 19:20:40 +0100

    Improved the parsing to be closer to the intended "grammar". I still need to tweak one last thing to make it work as intended.

--- src/main/Parser.hs
+++ src/main/Parser.hs
@@ -1,5 +1,5 @@
 import Text.ParserCombinators.Parsec -- AST for the language. This should really only be the list of legal tokens of the language. 
-import Control.Monad
+import Control.Monad hiding ((<|>))
 
 data Stabl = Word String    -- Word
                | Lit Int    -- Literal
@@ -7,17 +7,25 @@ data Stabl = Word String    -- Word
 
 -- Tester Parsec.......
 
+-- In the current implementation, a digit HAS TO be followed by some whitespace. This should be fixed so that it has to be followed by some whitespace, or by end of input.
 int :: Parser Stabl
-int = liftM (Lit . read) $ many1 digit
+int =  do
+       d <- liftM (Lit . read) $ many1 digit
+       _ <- skipMany1 space 
+       return d
+
+-- | Try parsing an int. If the parse fails, no input is consumed and no error is propagated. 
+tryInt :: Parser Stabl
+tryInt = try $ lookAhead int       
 
 word :: Parser Stabl
 word = liftM Word $ many1 alphaNum -- TODO: change to any character that is not whitespace?
 
 stablToken :: Parser Stabl
-stablToken = int <|> word -- a token that can't be parsed as an int literal is assumed to be a word
+stablToken =  tryInt <|> word -- a token that can't be parsed as an int literal is assumed to be a word
 
 parseStabl :: SourceName -> String -> Either ParseError [Stabl]
 parseStabl = do 
   program <- parse $ sepBy stablToken spaces 
-  _ <- parse $ many1 space :: SourceName -> String -> Either ParseError String-- Discard all whitespace at end of program
+  _ <- parse $ skipMany space :: SourceName -> String -> Either ParseError () -- Discard all whitespace at end of program
   return program
