a6f4bb92848a577f7afe3d6aa34715164960c42b
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2013-10-30 16:15:13 +0100

    Refactored Stack.hs (but now the usual way of evaluating an expression is harder. But I won't change it right now since I might change the whole evaluation-bit).

--- src/main/stack.hs
+++ src/main/stack.hs
@@ -20,29 +20,27 @@ type Token = String
 
 tokenize :: String -> [Token]
 tokenize = words
- 
-interpret :: String -> Int
-interpret str = let
-                tokens = tokenize str 
-                in interpret' (tokens,[])
-                   where 
-                     interpret' :: ([Token],[Int]) -> Int
-                     interpret' ([],stack) = head stack
-                     interpret' ((token:xs),stack)
-                       | all isDigit token = let num = read token in interpret' (xs,(num:stack))
-                       | all (`elem` "+-*/") token = 
-                         let (¤) = case token of
-                               "+" -> (+)
-                               "-" -> (-)
-                               "*" -> (*)
-                               "/" -> div
-                               _   -> error "invalid token: " ++ token
-                               in eval (¤) stack where 
-                                                       eval (¤) stack' = -- TODO: refactor this method into a more general one: one which takes an arbitary binary operator, a stack, and uses the two operands at the top of the stack to evaluate it (or throws stack underflow if there aren't at least two elements on the stack.)
-                                                         let x = head stack'
-                                                             y = head $ pop stack'
-                                                             res = x ¤ y
-                                                         in interpret' (xs,(res:(pop $ pop stack')))
+
+interpret :: ([Token],[Int]) -> Int
+interpret ([],stack) = head stack
+interpret ((token:xs),stack)
+  | all isDigit token = let num = read token in interpret (xs,(num:stack))
+  | all (`elem` "+-*/") token = 
+    let (¤) = case token of
+            "+" -> (+)
+            "-" -> (-)
+            "*" -> (*)
+            "/" -> div
+            _   -> error $ "invalid token: " ++ token
+                   in interpret (xs, eval stack (¤))
+                                                       
+ -- TODO: refactor this method into a more general one: one which takes an arbitary binary operator, a stack, and uses the two operands at the top of the stack to evaluate it (or throws stack underflow if there aren't at least two elements on the stack.)
+
+eval :: [Int] -> (Int -> Int -> Int) -> [Int]
+eval stack (¤) = let x = head stack
+                     y = head $ pop stack
+                     res = x ¤ y
+                 in res:(pop $ pop stack)
          
 
 
