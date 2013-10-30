0b9947066b202dfcb0536da7c61a17247649ac24
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2013-10-30 20:21:12 +0100

    The parser has a curious bug now: *** Exception: Text.ParserCombinators.Parsec.Prim.many: combinator 'many' is applied to a parser that accepts an empty string. . I suspect that this has to do with either 'notFollowedBy letter' or the lookaHead in tryInt.

--- src/main/Parser.hs
+++ src/main/Parser.hs
@@ -9,23 +9,25 @@ data Stabl = Word String    -- Word
 
 -- In the current implementation, a digit HAS TO be followed by some whitespace. This should be fixed so that it has to be followed by some whitespace, or by end of input.
 int :: Parser Stabl
-int =  do
-       d <- liftM (Lit . read) $ many1 digit
-       _ <- skipMany1 space 
-       return d
+int =  do 
+  i <- liftM (Lit . read) $ many1 digit 
+  _ <- notFollowedBy letter -- TODO: change? a string like "225+-" shouldn't be parsed by consuming "225" and discarding "+-". 
+  return i
+
+tryInt :: Parser Stabl
+tryInt = try $ lookAhead int
 
 -- | Try parsing an int. If the parse fails, no input is consumed and no error is propagated. 
-tryInt :: Parser Stabl
-tryInt = try $ lookAhead int       
 
 word :: Parser Stabl
-word = liftM Word $ many1 alphaNum -- TODO: change to any character that is not whitespace?
+word = liftM Word $ many1 alphaNum -- TODO: change to any character that is not whitespace? It should be possible to 
 
+-- | A token that can't be parsed as an int literal is assumed to be a word. If a token can't be parsed as an int, the lookahead fails without 1. consuming any input 2. propagating an error.  
 stablToken :: Parser Stabl
-stablToken =  tryInt <|> word -- a token that can't be parsed as an int literal is assumed to be a word
+stablToken =  tryInt <|> word 
 
 parseStabl :: SourceName -> String -> Either ParseError [Stabl]
 parseStabl = do 
-  program <- parse $ sepBy stablToken spaces 
+  program <- parse $ stablToken `sepBy` spaces 
   _ <- parse $ skipMany space :: SourceName -> String -> Either ParseError () -- Discard all whitespace at end of program
   return program
