e9f92f0c114a66ca9396d12371c8e48190950976
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2013-11-05 20:04:35 +0100

    I've added new parsers to take care of parsing things like quotations (example: [1 2 3 dup +]) and word definition (example: def sqr [dup *]). These parsers are incomplete in that they will give out errors if you give whitespace in places where it doesn't expect it; ideally superflous whitespaces should simply be ignored by the parser.

--- src/main/Parser.hs
+++ src/main/Parser.hs
@@ -8,12 +8,34 @@ module Parser
 import Text.ParserCombinators.Parsec -- AST for the language. This should really only be the list of legal tokens of the language. 
 import Control.Monad (liftM)
 
--- keywords
+-- defines a word (function). Syntax:
+-- def <name> (quotation or stablToken)
 def = "def"
+openQuot = "["
+closeQuot = "]";
 
-data Stabl = Word String    -- Word
+-- TODO: legg til alternativ: def <name> StablToken. Isåfall må eg oppdatere parseren.
+data WordDef = Def Word Quot 
+               deriving (Show,Read,Eq)
+
+type Word = String
+-- quotation. see: cat-lang
+type Quot = [Stabl]
+
+data Stabl = WordCall String    -- Word
                | Lit Int    -- Literal
                  deriving (Show,Read,Eq) -- TODO: meir?
+
+-- | A word definition
+wordDef :: Parser WordDef
+wordDef = do 
+  _ <- string def :: Parser String
+  name <- word'
+  quot' <- quotation
+  return $ Def name quot'
+
+quotation :: Parser Quot
+quotation = between (string openQuot) (string closeQuot) sequenceStablToken
 
 -- | The usual integer
 int :: Parser Stabl
@@ -24,17 +46,22 @@ int =  do
 
 -- | called "word prime" in order to be sure that it doesn't conflict
 -- | with any function named "word" in Parsec.
-word' :: Parser Stabl
-word' = liftM Word $ many1 alphaNum -- TODO: change to any character that is not whitespace? It should be possible to use strings like "+" and "-" as identifiers of words. 
+word' :: Parser Word
+word' = many1 alphaNum
+
+wordCall :: Parser Stabl
+wordCall = liftM WordCall $ word' -- TODO: change to any character that is not whitespace? It should be possible to use strings like "+" and "-" as identifiers of words. 
 
 -- | A token that can't be parsed as an int literal is assumed to be a word. If a token can't be parsed as an int, the lookahead fails without 1. consuming any input 2. propagating an error.  
 stablToken :: Parser Stabl
-stablToken =  (try int) <|> word'
+stablToken =  (try int) <|> wordCall
+
+sequenceStablToken = stablToken `sepBy` spaces 
 
 parseStabl :: SourceName -> String -> Either ParseError [Stabl]
 parseStabl = do
   _ <- discardWhitespace
-  program <- parse $ stablToken `sepBy` spaces 
+  program <- parse sequenceStablToken
   _ <- discardWhitespace  -- Discard all whitespace at end of program
   return program 
     -- TODO: found a bug: this doesn't seem to help with whitespace at start of input (returns empty list of tokens) nor at end of input (throws an exception). 
