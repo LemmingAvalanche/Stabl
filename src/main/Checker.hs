412481a5ef1614070a32b6b7b1b809f75f97a613
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2013-11-09 14:47:05 +0100

    I've updated Checker, mostly having to do with using Map to lookup word definitions.

--- src/main/Checker.hs
+++ src/main/Checker.hs
@@ -1,5 +1,6 @@
 -- | Type checking and general static semantic analysis.
 import Data.List
+import qualified Data.Map.Strict as Map
 
 import Parser
 
@@ -8,3 +9,12 @@ import Parser
 allUnique :: [WordDef] -> Bool
 allUnique list = (length unique) == (length list) 
   where unique = nubBy (\f s -> (wordName f) == (wordName s)) list
+
+-- | Make a list of WordDef into a list of tuples, so that it can easily be translated to a Map.
+zipDef :: [WordDef] -> [(Word, Quot)]
+zipDef list = zip 
+               [wordName def | def <- list] 
+               [wordQuot def | def <- list]
+
+toDict :: [WordDef] -> Map.Map Word Quot
+toDict = Map.fromList . zipDef
--- src/main/Parser.hs
+++ src/main/Parser.hs
@@ -1,7 +1,9 @@
 module Parser 
        (
          WordDef(..)
+       , Word
        , Stabl(..)
+       , Quot
        , parseDecl
        , parseStabl
        ) where
