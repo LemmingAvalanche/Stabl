9a38ad676c7450f41ebbb66ac20d2bc0f1947523
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2013-11-21 20:41:40 +0100

    Added more tests to TestParser, most importantly that have to do with testing if defining and parsing multiple definitions work.

---
+++ src/Stabl/Parser.hs
@@ -0,0 +1,92 @@
+module Parser 
+       (
+         WordDef
+       , Word
+       , Stabl(..)
+       , Quot
+       , parseDecl
+       , parseStabl
+       ) where
+
+import Text.ParserCombinators.Parsec hiding (many, (<|>))
+import Control.Applicative
+
+-- defines a word (function). Syntax:
+-- def <name> (quotation or stablToken) 
+def = "def"
+openDef = "{"
+closeDef = "}"
+
+openQuot = "["
+closeQuot = "]"
+
+-- TODO: legg til alternativ: def <name> StablToken. Isåfall må eg oppdatere parseren.
+-- TODO: add type declaration?
+-- data WordDef = Def {  wordName :: Word 
+--                    , wordQuot :: Quot
+--                    } deriving (Show,Read,Eq,Ord)
+
+type WordDef = (Word, Quot)
+
+-- TODO: give better name
+data Choice = Stabl | Quot deriving (Show,Read,Eq,Ord)
+
+type Word = String
+-- quotation. see: cat-lang
+type Quot = [Stabl]
+
+data Stabl = WordCall Word
+           | Quotation Quot
+           | Lit Int    -- Literal
+                 deriving (Show,Read,Eq,Ord) -- TODO: meir?
+
+-- | A word definition  
+wordDef :: Parser WordDef
+wordDef =  (string  def *> whitespace1)
+           *> liftA2 (,)
+           (word' <* whitespace1) 
+           definitionBody -- NOTE: eg skulle gjerne ha lagt til <|> stabl, altså at ein Def kan bestå av eit namn og ein quotation ELLER eit namn og ein stablToken
+           
+-- | Parser for declarations in a file. There has to be at least one declaration
+declarations :: Parser [WordDef]
+declarations = wordDef `sepBy` whitespace1
+
+parseDecl =  parse declarations -- NOTE: it seems that this parser will also accept word definitions that are come right after each other, ie without any whitespace. I use whitespace rather than whitespace1 because the latter would simply not parse word definitions that came up without a whitespace delimiting it, and that behaviour was more sneaky than this one.
+
+quotation :: Parser Quot
+quotation = between (string openQuot) (string closeQuot) sequenceStablToken
+
+-- | The same syntax as a quotation, only the def is enclosed by curly braces instead of square braces.
+definitionBody :: Parser Quot
+definitionBody = between (string openDef) (string closeDef) sequenceStablToken
+
+-- | The usual integer
+int :: Parser Stabl
+int =     fmap (Lit . read) $ many1 digit
+       <* notFollowedBy letter -- TODO: change? a string like "225+-" shouldn't be parsed by consuming "225" and discarding "+-". 
+
+
+-- | called "word prime" in order to be sure that it doesn't conflict
+-- | with any function named "word" in Parsec.
+word' :: Parser Word
+word' = many1 alphaNum
+
+wordCall :: Parser Stabl
+wordCall = fmap WordCall word' -- TODO: change to any character that is not whitespace? It should be possible to use strings like "+" and "-" as identifiers of words. 
+
+-- | A token that can't be parsed as an int literal is assumed to be a word. If a token can't be parsed as an int, the lookahead fails without 1. consuming any input 2. propagating an error.  
+stablToken :: Parser Stabl
+stablToken =  (try int) <|> wordCall
+
+sequenceStablToken =    whitespace         
+                     *> stablToken `sepBy` whitespace1
+                     <* whitespace -- BUG: gives exception if there is whitespace at end of string 
+
+whitespace = many space
+
+whitespace1 :: Parser String
+whitespace1 = many1 space
+
+parseStabl :: SourceName -> String -> Either ParseError [Stabl]
+parseStabl = parse sequenceStablToken
+
---
+++ src/Stabl/TestParser.hs
@@ -0,0 +1,46 @@
+module TestParser
+       (
+         testDef1GivesNoError
+       , testDef1ParsesCorrectly
+       ) where
+import Test.HUnit
+import Data.Either
+
+import Parser
+
+name = "name"
+
+def1 = "def " ++ name ++ " {1 2 3 4 5}"
+expectedDef1 = [(name, [Lit 1,Lit 2,Lit 3,Lit 4,Lit 5])]
+
+def2 = "def " ++ name ++ " {hei 2 3 4 hade}"
+expectedDef2 = [(name, [WordCall "hei", Lit 2, Lit 3, Lit 4, WordCall "hade"])]
+
+defList1 = def1 ++ "       " ++ def2
+expectedDefList1 = expectedDef1 ++ expectedDef2
+
+defList2 = def1 ++ "   " ++ def2 ++ "  " ++ def1
+expectedDefList2 = expectedDef1 ++ expectedDef2 ++ expectedDef1
+
+-- All tests
+tests = TestList [TestLabel "test1" testDef1GivesNoError, TestLabel "test2" testDef1ParsesCorrectly, TestLabel "test3" testDef1ParsesCorrectly, TestLabel "test4" testDefList1ParsesCorrectly, TestLabel "test5" testDefList2ParsesCorrectly]
+
+testDef1GivesNoError = TestCase $ assertBool 
+                      "The definition gave an error" 
+                      (case (parseDecl "" def1) 
+                                 of Right _ -> True
+                                    Left _ -> False)
+                      
+testDef1ParsesCorrectly = testParseAgainstExpected def1 expectedDef1
+
+testDef2ParsesCorrectly = testParseAgainstExpected def1 expectedDef2
+
+testDefList1ParsesCorrectly = testParseAgainstExpected defList1 expectedDefList1
+
+testDefList2ParsesCorrectly = testParseAgainstExpected defList2 expectedDefList2
+
+testParseAgainstExpected str expected = TestCase $ assertEqual "def1 was not parsed correctly" (getRight $ parseDecl "" str) expected
+
+-- | Unsafely gets the Right value. 
+getRight :: Either a b -> b
+getRight (Right value) = value
