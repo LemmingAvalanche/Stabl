34fac343e8543655ee3cc701031a4a763d466873
Kristoffer Haugsbakk <haugsbakk@yahoo.no>
2013-12-13 19:21:29 +0100

    Updated all functions (to my knowledge) to be total: now they return sensible errors as part of their types when they encounter an erroneus state.

--- src/Stabl/Interpreter.hs
+++ src/Stabl/Interpreter.hs
@@ -5,48 +5,78 @@ module Interpreter
        , apply
        ) where
 import Control.Applicative
+import Control.Monad.Error
 import qualified Data.Map as Map
 
 import Parser
+import Text.ParserCombinators.Parsec.Error
 
-top [] = Nothing
-top (x:_) = x
+data StablErr = StackUnderflow String
+                | Parser ParseError
+                | TypeMismatchErr { 
+                    expected :: String
+                  , actual :: String }
+                | UndefinedWord String
+                | Default String
 
-pop [] = Nothing
-pop (x:xs) = Just xs
+instance Error StablErr where
+  noMsg = Default "An error has occured"
+  strMsg = Default
+
+showErr (StackUnderflow str) = "Stack underflow: the combinator " ++ str ++ " was called on the stack, but there were not enough elements on the stack."
+showErr (Parser err) = show err
+showErr (TypeMismatchErr exp act) = "Typemismatch error: expected " ++ exp ++ ", but got " ++ act
+showErr (UndefinedWord str) = "Undefined word error: tried to call word " ++ str ++ ", but it is not defined."
+
+type CanErr = Either StablErr
+
+type Dict = Map.Map String [Stabl]
+
+top :: [Stabl] -> CanErr Stabl
+top [] = Left $ StackUnderflow "top"
+top (x:_) = Right x
+
+pop :: [Stabl] -> CanErr [Stabl]
+pop [] = Left $ StackUnderflow "pop"
+pop (x:xs) = Right xs
 
 -- | See - Forth
-dup [] = Nothing
-dup (x:xs) = Just $ x:x:xs
+dup :: [Stabl] -> CanErr [Stabl]
+dup [] = Left $ StackUnderflow "dup"
+dup (x:xs) = Right $ x:x:xs
 
 -- | See - Forth
-swap [] = Nothing
-swap [_] = Nothing
-swap (x:y:xs) = Just $ y:x:xs
+swap :: [Stabl] -> CanErr [Stabl]
+swap [] = Left $ StackUnderflow "swap"
+swap [_] = Left $ StackUnderflow "swap"
+swap (x:y:xs) = Right $ y:x:xs
+
+-- | See - Forth 
+rot :: [Stabl] -> CanErr [Stabl]
+rot [] = Left $ StackUnderflow "rot"
+rot [_] = Left $ StackUnderflow "rot"
+rot [_,_] = Left $ StackUnderflow "rot"
+rot (x:y:z:xs) = Right $ z:x:y:xs
 
 -- | See - Forth
-rot [] = Nothing
-rot [_] = Nothing
-rot [_,_] = Nothing
-rot (x:y:z:xs) = Just $ z:x:y:xs
+over :: [Stabl] -> CanErr [Stabl]
+over [] = Left $ StackUnderflow "over"
+over [_] = Left $ StackUnderflow "over"
+over (x:y:xs) = Right $ y:x:y:xs
 
--- | See - Forth
-over [] = Nothing
-over [_] = Nothing
-over (x:y:xs) = Just $ y:x:y:xs
-
-apply :: [Stabl] -> Map.Map String [Stabl] -> [Stabl] -> Either String [Stabl]
+apply :: [Stabl] -> Dict -> [Stabl] -> CanErr [Stabl]
 apply stack dict quot = interpret ((reverse quot) ++ stack) dict
-
+ 
 -- TODO: fix to make total
-parseCheckAndInterpret :: String -> Map.Map String [Stabl] -> Either String [Stabl]
-parseCheckAndInterpret s dict = either 
-                                (\err -> Left $ "Parse error: " ++ (show err)) 
+parseCheckAndInterpret :: String -> Dict -> CanErr [Stabl]
+parseCheckAndInterpret s dict = either
+                                (Left . Parser)
                                 (\prog -> interpret prog dict) 
                                 (parseStabl "" s)
 
+
 -- | interpret a program given by a quotation.
-interpret :: [Stabl] -> Map.Map String [Stabl] -> Either String [Stabl]
+interpret :: [Stabl] -> Dict -> CanErr [Stabl]
 interpret s dict = interpret' (s, dict , [])
 
 -- Data stack
@@ -56,11 +86,13 @@ interpret s dict = interpret' (s, dict , [])
 -- Result stack
 -- newtype Result a = Result { getResult :: [a] }
 
-interpret' :: ([Stabl], Map.Map String [Stabl], [Stabl]) -> Either String [Stabl]
+interpret' :: ([Stabl], Dict, [Stabl]) -> CanErr [Stabl]
 interpret' ([], dict, stack) = case (head stack) of 
   Lit num    -> Right stack
   Quotation quot' -> Right stack
-  WordCall w -> Left $ "type error: the returning stack contained an unevaluated word: " ++ w
+  WordCall w -> Left $ TypeMismatchErr {
+    expected = "a stack with only values"
+    , actual = "an unevaluated word"}
 interpret' (Lit n : xs, dict, stack) = interpret' (xs, dict, Lit n : stack)
 interpret' (Quotation quot : xs, dict, stack) = interpret' (xs, dict, Quotation quot : stack)
 interpret' (WordCall s : xs, dict, stack) = 
@@ -81,12 +113,16 @@ interpret' (WordCall s : xs, dict, stack) =
             
             -- applying a quotation 
             "apply"   -> case head' of Quotation quot -> eitherR (\res -> interpret' (xs, dict, res)) (apply quot dict tail')
-                                       other -> Left $ "expected a quotation for word \"apply\", but was instead " ++ (show other)
+                                       other -> Left $ TypeMismatchErr {
+                                         expected = "a quotation"
+                                         , actual = "the word: " ++ (show other)}
               where head' = head stack
                     tail' = tail stack
             -- user-defined word 
-            other   -> case lookup' of Just wordBody -> eitherR (\res -> interpret' (xs, dict, res)) (apply wordBody dict stack) 
-                                       Nothing       -> Left $ "undefined word: " ++ other 
+            other   -> case lookup' of Just wordBody -> eitherR 
+                                                        (\res -> interpret' (xs, dict, res)) 
+                                                        (apply wordBody dict stack) 
+                                       Nothing       -> Left $ UndefinedWord other
               where lookup' = Map.lookup other dict
 
 -- TODO: move to where-clase in interpret'?
@@ -94,19 +130,19 @@ ifSuccessArithmetic stack' dict' op = eitherR
                                       (\res -> interpret' (stack', dict', res)) 
                                       (eval stack' op)
 
-ifSuccessComb stack dict retStack comb = maybe 
-                                         (Left "Stack Underflow Error.")
+ifSuccessComb stack dict retStack comb = eitherR
                                          (\res -> interpret' (stack, dict, res)) 
                                          (comb retStack)
 
-eval :: [Stabl] -> (Int -> Int -> Int) -> Either String [Stabl]
-eval stack (¤) = let x = top stack
-                     y = top $ pop stack 
-                     res = liftA2 (¤) (get y) (get x) :: Either String Int
-                       where get t = case t of 
-                               WordCall s -> Left $ "was a word, expected num: " ++ s 
-                               Lit n -> Right n
-                 in eitherR (\result -> Right $ (Lit result):(pop $ pop stack)) res
+eval :: [Stabl] -> (Int -> Int -> Int) -> CanErr [Stabl]
+eval stack op = let x = top stack
+                    y = eitherR top (pop stack) 
+                    res = liftA2 op (get y) (get x) :: CanErr Int
+                       where get = eitherR 
+                                   (\res -> case res of 
+                                                   WordCall s -> Left $ TypeMismatchErr { expected = "an int", actual = "the word: " ++ s }
+                                                   Lit n -> Right n)
+                 in eitherR (\result -> Right $ (Lit result):stack'') res where stack'' = tail $ tail stack -- Uses partial functions, BUT, this is safe since the evaluation can't have come this far if there indeed weren't at least two items on top of the stack, since then we would not have been able to use arithmetic on the top two elements of the stack. 
 
 -- | Evaluates the right value in the first given function if Right value and returns an either value; propagates the error if Left value
 eitherR :: (b -> Either a c) -> Either a b -> Either a c
