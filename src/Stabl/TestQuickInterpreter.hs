72a8a49a9cb2da49dcf700403983da1dc18c27e3
LemmingsAvalanche <haugsbakk@yahoo.no>
2014-07-26 20:48:58 +0200

    Added proper quickcheck for arithmetic division.

--- src/Stabl/TestQuickInterpreter.hs
+++ src/Stabl/TestQuickInterpreter.hs
@@ -8,10 +8,12 @@ helper_arith1 x y op opString = let expr = show x ++ " " ++ show y ++ " " ++ opS
                                     in let expected = x `op` y
                                            actual = parseAndInterpret expr 
                                        -- TODO : refactor
-                                       in case actual of Left _ -> True
+                                       in case actual of Left _ -> False
                                                          Right actual' -> LitInt expected == head actual'
 
 prop_arith_add x y = helper_arith1 x y (+) "+"
 prop_arith_minus x y = helper_arith1 x y (-) "-"
 prop_arith_mul x y = helper_arith1 x y (*) "*"
--- prop_arith_div x y = helper_arith1 x y div "div" -- How should I deal with tests failing because of division by zero?
+prop_arith_div x y = if y /= 0 
+                     then helper_arith1 x y div "/"
+                     else True -- How should I deal with tests failing because of division by zero?
