011ed4f20000754fdffe1a12e02679a53a11642d
LemmingsAvalanche <haugsbakk@yahoo.no>
2014-07-28 19:39:37 +0200

    Fixed bug in combinator test that was added in the previous commit. The bug remained hidden (the code compiled, and the tests passed) due to lack of a type signature on the test: once it was added, the bug was revealed. I'm not quite sure why that was the case.

--- src/Stabl/TestQuickInterpreter.hs
+++ src/Stabl/TestQuickInterpreter.hs
@@ -13,7 +13,7 @@ check expected = either
 
 helper_arith1 :: Integer -> Integer -> (Integer -> Integer -> Integer) -> String -> Bool
 helper_arith1 x y op opString = let expr = show x ++ " " ++ show y ++ " " ++ opString 
-                                    expected = LitInt $ x `op` y
+                                    expected = LitInt $ x `op` x
                                     actual = fmap head $ parseAndInterpret expr 
                                 in check expected actual
 
@@ -33,7 +33,8 @@ swap = "swap"
 rot = "rot"
 over = "over"
 
-prop_pop1 x = let expr = show x ++ pop
+prop_pop1 :: Int -> Bool
+prop_pop1 x = let expr = show x ++ " " ++ pop
                   expected = []
                   actual = parseAndInterpret expr
               in check expected actual
