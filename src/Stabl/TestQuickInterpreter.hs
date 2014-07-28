8653d623f3cfee116422ab2068864ac86074a29d
LemmingsAvalanche <haugsbakk@yahoo.no>
2014-07-28 19:28:54 +0200

    Added a test for testing the pop combinator. Also refactored the test file.

--- src/Stabl/TestQuickInterpreter.hs
+++ src/Stabl/TestQuickInterpreter.hs
@@ -3,13 +3,19 @@ import Test.QuickCheck
 import Parser
 import Interpreter
 
+-- Help
+
+check expected = either
+                 (const False)
+                 (==expected)
+
+-- Arithmetic
+
 helper_arith1 :: Integer -> Integer -> (Integer -> Integer -> Integer) -> String -> Bool
 helper_arith1 x y op opString = let expr = show x ++ " " ++ show y ++ " " ++ opString 
-                                    in let expected = x `op` y
-                                           actual = parseAndInterpret expr 
-                                       -- TODO : refactor
-                                       in case actual of Left _ -> False
-                                                         Right actual' -> LitInt expected == head actual'
+                                    expected = LitInt $ x `op` y
+                                    actual = fmap head $ parseAndInterpret expr 
+                                in check expected actual
 
 prop_arith_add x y = helper_arith1 x y (+) "+"
 prop_arith_minus x y = helper_arith1 x y (-) "-"
@@ -17,3 +23,18 @@ prop_arith_mul x y = helper_arith1 x y (*) "*"
 prop_arith_div x y = if y /= 0 
                      then helper_arith1 x y div "/"
                      else True 
+
+-- Combinators
+
+-- TODO: add these strings to Interpreter.hs instead and export them as part of the module.
+pop = "pop"
+dup = "dup"
+swap = "swap"
+rot = "rot"
+over = "over"
+
+prop_pop1 x = let expr = show x ++ pop
+                  expected = []
+                  actual = parseAndInterpret expr
+              in check expected actual
+                                
